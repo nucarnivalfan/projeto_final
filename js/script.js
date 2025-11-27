b8f0866cc88e0dcf6b13208d73802c3059d2a51f
Guilherme Firmino da Silva. <guilhermefsilva.09012009@gmail.com>
2025-11-27 16:56:29 -0300

    upload-mobile_first_feito-GuilhermeF
    feito a parte do mobile first

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ function mudarTamanho() {
     if (window.innerwidth >=768) {
         itens.style.display = 'inline-block'
     } else {
-        itens.style
+        itens.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
     }
 }
