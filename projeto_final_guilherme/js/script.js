7491a147e1c156f3e7bf7c3bc3209593cb4c3026
Guilherme Firmino da Silva. <guilhermefsilva.09012009@gmail.com>
2025-11-28 16:59:24 -0300

    upload-arrumação_do_codigo_e_daylight-guilhermef

--- projeto_final_guilherme/js/script.js
+++ projeto_final_guilherme/js/script.js
@@ -15,3 +15,27 @@ function clickMenu() {
         itens.style.display = 'block'
     }
 }
+
+const btnDarkMode = document.getElementById('btn-dark-mode');
+
+const themeSystem = localStorage.getItem('themeSystem') || 'light';
+
+btnDarkMode.addEventListener('click',() => {
+    let oldTheme = localStorage.getItem('themeSystem') || 'light';
+    let newTheme =oldTheme === "light" ? "dark" :"light";
+    localStorage.setItem('themeSystem', newTheme);
+    defineCurrentTheme(newTheme);
+});
+
+function defineCurrentTheme(theme) {
+    const darkIcon = '<span class="material-symbols-rounded">light_mode</span>';
+    const lightIcon = '<span class="material-symbols-rounded">dark_mode</span>';
+    document.documentElement.setAttribute("data-theme",theme);
+        if  (theme === "light") {
+            btnDarkMode.innerHTML = darkIcon;
+          return;
+        }
+    btnDarkMode.innerHTML = lightIcon;
+}
+
+defineCurrentTheme(themeSystem);
